6c25e6678c3b91bf410411589b8d3f815bf2902b
mb <michael_blanchard@hms.harvard.edu>
2022-06-08 16:24:41 -0400

    added many utils helper functions, reconstruct retile.py

--- retile.py
+++ retile.py
@@ -1,24 +1,29 @@
 import numpy as np
 import ezomero as ez
-from utils import getTile, roiTiler, nearestMultipleOf
+from utils import getTile, roiTiler, nearestMultipleOf, getProjectPixels, getDimensionMap,getProjectImages
 from ezomero import rois
 from math import ceil
 
 TILE_DIM = 5000 #dimension of resulting tiles
+PROJECT_ID = 9152
 
 def main():
     conn = ez.connect(config_path=".")
-    image = conn.getObject("Image", 1654601)
-    pixels = image.getPrimaryPixels()
-    tile_arr = pixels.getTile(0,0,0,(0,0,5000,5000))
-    d1 = int(image.getSizeX())
-    d2 = int(image.getSizeY())
-    col, row = nearestMultipleOf(TILE_DIM, d1, d2)
-    rect_list = roiTiler(TILE_DIM, row, col)
-    ez.post_roi(conn,1654601, rect_list, "test roi1")
-    
+    image_ids = ez.get_image_ids(conn, project=PROJECT_ID)
+    pixellist = getProjectPixels(conn, image_ids)
+    imagelist = getProjectImages(conn, image_ids)
+    imagedict = getDimensionMap(conn, imagelist, TILE_DIM)
+        
 
-    ##row, col = nearestMultipleOf(TILE_DIMENSION, d1, d2)
+    # image = conn.getObject("Image", 1654601)
+    # pixels = image.getPrimaryPixels()
+    # tile_arr = pixels.getTile(0,0,0,(0,0,5000,5000))
+    # d1 = int(image.getSizeX())
+    # d2 = int(image.getSizeY())
+    # col, row = nearestMultipleOf(TILE_DIM, d1, d2)
+    # rect_list = roiTiler(TILE_DIM, row, col)
+    # ez.post_roi(conn,1654601, rect_list, "test roi1")
+    # ##row, col = nearestMultipleOf(TILE_DIMENSION, d1, d2)
 
     conn.close()
 
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 from math import ceil
 from ezomero import rois
+import time
 
 def nearestMultipleOf(tile_dim, dim1, dim2) -> tuple:
     return int(ceil(dim1/tile_dim)), int(ceil(dim2/tile_dim))
@@ -12,5 +13,31 @@ def roiTiler(tile_dim, row, col,) -> list:
             rect_list.append(rois.Rectangle(x*tile_dim, y*tile_dim, tile_dim, tile_dim, label="row: %d col: %d" % (y, x)))
     return rect_list
 
+#get tile segment of image
 def getTile(pixels, row, col, tile_dim) -> np.array:
     return pixels.getTile(0,0,0,(col*tile_dim,row*tile_dim,tile_dim,tile_dim))
+
+#get all pixel objects from project images
+def getProjectPixels(conn, id_list) -> list:
+    pixellist = []
+    for id in id_list:
+        image = conn.getObject("Image", id)
+        pixellist.append(image.getPrimaryPixels())
+    return pixellist
+
+#get all images from project
+def getProjectImages(conn, id_list) -> list:
+    imagelist = []
+    for id in id_list:
+        imagelist.append(conn.getObject("Image", id))
+    return imagelist
+
+#buid a dict of all images with required rows and columns 
+def getDimensionMap(conn, id_list, tile_dim) -> dict:
+    imagedict = {}
+    for id in id_list:
+        col, row = nearestMultipleOf(tile_dim, id.getSizeX(), id.getSizeY())
+        key_val = {id.getId():(col,row)}
+        imagedict.update(key_val)
+    return imagedict
+    
