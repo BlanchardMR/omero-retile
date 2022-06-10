4acd27a8a51bfa4c7c668d2705e930970adcf41f
mb <michael_blanchard@hms.harvard.edu>
2022-06-10 10:37:51 -0400

    improved utils.py documentation

--- utils.py
+++ utils.py
@@ -4,40 +4,113 @@ from ezomero import rois
 import ezomero as ez
 import time
 
-def nearestMultipleOf(tile_dim, col, row) -> tuple:
-    return int(col/tile_dim), int(row/tile_dim)
+def tileMultiple(tile_dim, pixel_x, pixel_y) -> tuple:
+    """ Function used to calcualte the number of rows and columns in parent image
+    
+    Parameters
+    ----------
+    tile_dim : int
+    pixel_x : int
+    pixel_y : int
+
+    Returns
+    --------
+    tuple (int x, int y) 
+
+    """
+    return int(pixel_x/tile_dim), int(pixel_y/tile_dim)
 
 def roiTiler(tile_dim, row, col,) -> list:
+    """ Function to create rectangle roi list for visual mapping of tiles in omero db
+    
+    Parameters
+    ----------
+    tile_dim : int
+    row : int
+    col : int
+
+    Returns
+    --------
+    list(<ezomero.rois.Rectangle>) 
+    """
     rect_list = []
     for y in range(0, row):
         for x in range(0, col):
             rect_list.append(rois.Rectangle(x*tile_dim, y*tile_dim, tile_dim, tile_dim, label="row: %d col: %d" % (y, x)))
     return rect_list
 
-#get tile segment of image
+
 def get_tile(pixels, row, col, tile_dim, z=0,c=0, t=0) -> np.array:
+    """ wrapper function find and return tile by col, row location at given tile dimension
+    
+    Parameters
+    ----------
+    pixels : <Omero.Pixel> ??? need to correct this
+    row : int
+    col : int
+    tile_dim: int
+    z = int
+    c = int
+    t = int
+
+    Returns
+    --------
+    numpy.ndArray : 2 Dimensions regardless of color space
+    """
     return pixels.getTile(z, c, t,(col*tile_dim,row*tile_dim,tile_dim,tile_dim))
 
-#get all pixel objects from project images
+
 def getProjectPixels(conn, id_list) -> list:
+    """ wrapper function find and return tile by col, row location at given tile dimension
+    
+    Parameters
+    ----------
+    conn = <Blitzgateway.connection>
+    id_list = [int]
+    
+    Returns
+    --------
+    list[<Omero.pixels>] ??? double check this object type
+    """
     pixellist = []
     for id in id_list:
         image = conn.getObject("Image", id)
         pixellist.append(image.getPrimaryPixels())
     return pixellist
 
-#get all images from project
 def getProjectImages(conn, id_list) -> list:
+    """ builds list of all image id numbers in project
+    
+    Parameters
+    ----------
+    conn = <Blitzgateway.connection>
+    id_list = [int]
+    
+    Returns
+    --------
+    lis[int]
+    """
     imagelist = []
     for id in id_list:
         imagelist.append(conn.getObject("Image", id))
     return imagelist
 
-#buid a dict of all images with required rows and columns 
-def getDimensionMap(id_list, tile_dim) -> dict:
+
+def dimensionGridList(id_list, tile_dim) -> dict:
+    """ build dictionary of key: image id, value: (columns, rows)
+    
+    Parameters
+    ----------
+    id_list = [int]
+    tile_dim = int
+    
+    Returns
+    --------
+    dict{key: int val: tuple(int,int)}
+    """
     imagedict = {}
     for id in id_list:
-        col, row = nearestMultipleOf(tile_dim, id.getSizeX(), id.getSizeY())
+        col, row = tileMultiple(tile_dim, id.getSizeX(), id.getSizeY())
         key_val = {id.getId():(col,row)}
         imagedict.update(key_val)
     return imagedict
