c2403e24e629cc5fde7e3c39a16f953852a064a2
mb <michael_blanchard@hms.harvard.edu>
2022-06-13 16:21:38 -0400

    added single image tile command line arguments

--- src/retile.py
+++ src/retile.py
@@ -2,7 +2,7 @@
 import sys, getopt
 import numpy as np
 import ezomero as ez
-from utils import get_tile, roiTiler, tileMultiple, getProjectPixels, dimensionGridList,getProjectImages, makeTileRGB
+from utils import tileSingleImage, roiTiler, tileMultiple, getProjectPixels, dimensionGridList,getProjectImages, makeTilesRGB
 from ezomero import rois
 from math import ceil
 import omero
@@ -13,21 +13,27 @@ PROJECT_ID = 9152
 def main(argv):
     project_id = ''
     image_id = ''
+    dataset = ''
     tile_dim = 2048
+    conn = ez.connect(config_path=".")  #config path is lo
+    ##need to setup combined argument outcomes ex. : -p + -d + -i
     try:
-        opts, args = getopt.getopt(argv,"tp:di",["tile_dim=","project_id=", "dataset=","image_id="])
+        opts, args = getopt.getopt(argv,"tpdi:",["tile_dim=","project_id=", "dataset=","image_id="])
     except getopt.GetoptError:
-      print('retile.py -p <project_id>')
+      print('retile.py -p <project_id> -d <dataset> -i <image id>')
       sys.exit(2)
     for opt, arg in opts:
-      if opt == '-d':
+      if opt == '-t':
          print ('dimension set to', arg)
       elif opt == '-p':
         project_id = arg
+      elif opt == '-d':
+        dataset = arg
       elif opt == '-i':
-         image_id = arg
+        image_id = arg
+        image = conn.getObject("Image", str(image_id))
+        tileSingleImage(conn, image, tile_dim)
 
-    conn = ez.connect(config_path=".")  #config path is location of .ezomero file
     image_ids = ez.get_image_ids(conn, dataset=18352)
     imagelist = getProjectImages(conn, image_ids)
     imagedict = dimensionGridList(imagelist, tile_dim)
@@ -38,7 +44,7 @@ def main(argv):
         ez.post_roi(conn, k, roiTiler(tile_dim, v[2], v[1]))
         for y in range(v[2]):
             for x in range(v[1]):   
-                tile = makeTileRGB(v[0], x, y, tile_dim)
+                tile = makeTilesRGB(v[0], x, y, tile_dim)
                 print(ez.post_image(conn, image=np.flipud(np.rot90(tile, k=1, axes=(0,1))), image_name=str(k) +"_tile_" + str(y) +"_" + str(x), source_image_id=k, channel_list=[0,1,2], dim_order="xyzct" ))
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,10 @@
 
 import numpy as np
+import ezomero as ez
 from ezomero import rois
 
 
-def tileMultiple(tile_dim, pixel_x, pixel_y) -> tuple:
+def tileMultiple(image, tile_dim) -> tuple:
     """ Function used to calcualte the number of rows and columns in parent image
     
     Parameters
@@ -17,6 +18,8 @@ def tileMultiple(tile_dim, pixel_x, pixel_y) -> tuple:
     tuple (int x, int y) 
 
     """
+    pixel_x = image.getSizeX()
+    pixel_y = image.getSizeY()
     return int(pixel_x/tile_dim), int(pixel_y/tile_dim)
 
 def roiTiler(tile_dim, row, col,) -> list:
@@ -39,7 +42,7 @@ def roiTiler(tile_dim, row, col,) -> list:
     return rect_list
 
 
-def get_tile(pixels, row, col, tile_dim, z=0,c=0, t=0) -> np.array:
+def get_tile(image, row, col, tile_dim, z=0,c=0, t=0) -> np.array:
     """ wrapper function find and return tile by col, row location at given tile dimension
     
     Parameters
@@ -56,11 +59,13 @@ def get_tile(pixels, row, col, tile_dim, z=0,c=0, t=0) -> np.array:
     --------
     numpy.ndArray : 2 Dimensions regardless of color space
     """
+    pixels = image.getPrimaryPixels()
     return pixels.getTile(z, c, t,(col*tile_dim,row*tile_dim,tile_dim,tile_dim))
 
 
 def getProjectPixels(conn, id_list) -> list:
     """ wrapper function find and return tile by col, row location at given tile dimension
+    This function is now useless but I'm keeping it around JIC
     
     Parameters
     ----------
@@ -109,15 +114,14 @@ def dimensionGridList(image_list, tile_dim) -> dict:
     """
     imagedict = {}
     for image in image_list:
-        print(str(image.getSizeX()) + " " + str(image.getSizeY()))
-        col, row = tileMultiple(tile_dim, image.getSizeX(), image.getSizeY())
+        col, row = tileMultiple(image, tile_dim)
         if col < 1 or row < 1: continue
-        key_val = {image.getId():(image.getPrimaryPixels(),col,row)}
+        key_val = {image.getId():(image,col,row)}
         imagedict.update(key_val)
     return imagedict
     
 
-def makeTileRGB(primary_pixels, x, y, tile_dim) -> np.ndarray:
+def makeTilesRGB(image, x, y, tile_dim) -> np.ndarray:
     """ from pixels create tile for each channel of RGB image and build new 
     
     Parameters
@@ -130,10 +134,10 @@ def makeTileRGB(primary_pixels, x, y, tile_dim) -> np.ndarray:
     --------
     nd.array [x,y,z,c,t]
     """
-    print("STARTING makeTileRGB")
-    tileR = get_tile(primary_pixels, y, x, tile_dim, c=0)
-    tileG = get_tile(primary_pixels, y, x, tile_dim, c=1)
-    tileB = get_tile(primary_pixels, y, x, tile_dim, c=2)
+    print(("creating tile x:%d, y:%d") %(x, y))
+    tileR = get_tile(image, y, x, tile_dim, c=0)
+    tileG = get_tile(image, y, x, tile_dim, c=1)
+    tileB = get_tile(image, y, x, tile_dim, c=2)
     tileR = np.expand_dims(tileR, axis=(2,3,4))
     tileG = np.expand_dims(tileG, axis=(2,3,4))
     tileB = np.expand_dims(tileB, axis=(2,3,4))
@@ -161,4 +165,20 @@ def makeTileGScale(primary_pixels, x, y, tile_dim, size_c) -> np.ndarray:
         tile = np.expand_dims(tile, axis=(2,3,4))
         np.append(tile_r, tile)
     return tile_r
+
+def tileSingleImage(conn, image, tile_dim):
+    id = image.getId()
+    col_x, col_y = tileMultiple(image, tile_dim)
+    for y in range(col_y):
+        for x in range(col_x):
+            tile = makeTilesRGB(image, x, y, tile_dim)
+            ez.post_image(
+                  conn=conn
+                , image=np.flipud(np.rot90(tile, k=1, axes=(0,1)))
+                , image_name=str(id) +"_tile_" + str(y) +"_" + str(x)
+                , source_image_id=id
+                , channel_list=[0,1,2]
+                , dim_order="xyzct" 
+                )
+ 
 
